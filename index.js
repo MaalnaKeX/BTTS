a757e052fa04230131a9e76a4d4535090b0d4581
MaalnaKeX <75688160+MaalnaKeX@users.noreply.github.com>
2022-11-12 17:24:22 +0100

    Fixed Javascript

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const main = document.getElementsByTagName('main');
+function init(){
+    const content = document.getElementById("content");
+    const title = document.getElementById("title");
+};
 
-function changeMain(tab) {
-    if (tab == 'info') {
-        main.innerHTML = 'info_html';
-    } else if (tab == 'matek') {
-        main.innerHTML = 'mat_html';
-    } else if (tab == 'fizika') {
-        main.innerHTML = 'fiz_html';
-    } else if (tab == 'tori') {
-        main.innerHTML = 'tor_html';
-    } else if (tab == 'magyar') {
-        main.innerHTML = 'magy_html';
-    } else if (tab == 'angol') {
-        main.innerHTML = 'ang_html';
-    } else if (tab == 'nemet') {
-        main.innerHTML = 'nem_html';
-    }
+window.onload = init;
+
+function changeMain(tab, obj) {
+    // if (tab == 'info') {
+    //     main.innerHTML = 'info_html';
+    // } else if (tab == 'matek') {
+    //     main.innerHTML = 'mat_html';
+    // } else if (tab == 'fizika') {
+    //     main.innerHTML = 'fiz_html';
+    // } else if (tab == 'tori') {
+    //     main.innerHTML = 'tor_html';
+    // } else if (tab == 'magyar') {
+    //     main.innerHTML = 'magy_html';
+    // } else if (tab == 'angol') {
+    //     main.innerHTML = 'ang_html';
+    // } else if (tab == 'nemet') {
+    //     main.innerHTML = 'nem_html';
+    // }
+    title.innerHTML = obj.innerHTML;
+    console.log(tab);
 }
